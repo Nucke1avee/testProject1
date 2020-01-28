84251cc155a636348cdf894bf29ba825e90f2ecc
Nucke1avee <8709998@gmail.com>
2020-01-28 17:12:01 +0300

    280120

--- src/org/jakumo/euler/e3/Main.java
+++ src/org/jakumo/euler/e3/Main.java
@@ -1,10 +1,15 @@
 package org.jakumo.euler.e3;
 //Простые делители числа 13195 - это 5, 7, 13 и 29.
 //Каков самый большой делитель числа 600851475143, являющийся простым числом?
-
 public class Main {
     public static void main(String[] args) {
+        long num = 600851475143L;
+        int maxDel = 0;
 
+
+
+
+        System.out.println("\nResult: " + maxDel);
     }
 
     public static boolean isSimple(int i) {
