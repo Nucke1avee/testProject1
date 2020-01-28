3f2211645d834500d329e8f998ec1a940f00b670
Nucke1avee <8709998@gmail.com>
2020-01-28 16:12:38 +0300

    it looks better now) i hope so...

--- src/org/jakumo/euler/e2/Main.java
+++ src/org/jakumo/euler/e2/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         for (int i = 0; a[i % 3] < 4000000; i++) {
             if (a[i % 3] % 2 == 0) sum += a[i % 3];
-            System.out.print(a[i % 3] + " ");
+            //System.out.print(a[i % 3] + " ");
             a[i % 3] = a[(i + 1) % 3] + a[(i + 2) % 3];
         }
         System.out.println("\nResult: " + sum);
