b8643e408bb177d38881a635208dfc403db6507b
Nucke1avee <8709998@gmail.com>
2020-01-30 17:47:46 +0300

    30012020

--- src/org/jakumo/euler/e6/Main.java
+++ src/org/jakumo/euler/e6/Main.java
@@ -10,6 +10,17 @@ package org.jakumo.euler.e6;
   Найдите разность между суммой квадратов и квадратом суммы первых ста натуральных чисел.*/
 public class Main {
     public static void main(String[] args) {
+        //execution time ~25ms
+        int n = 100;
+        int sk = 0, ks = 0;
 
+        for (int i = 1; i <= n; i++) {
+            sk += i * i;
+        }
+        for (int i = 1; i <= n; i++) {
+            ks += i;
+        }
+        ks *= ks;
+        System.out.println("Result: " + (ks - sk));
     }
 }
