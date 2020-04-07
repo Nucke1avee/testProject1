c2d2c4743edf7acf89ed753009420908e701ae53
NCK1V <666lion@gmail.com>
2020-04-07 18:17:30 +0300

    070420

--- src/testField/TestField.java
+++ src/testField/TestField.java
@@ -1,18 +1,32 @@
 package testField;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class TestField {
-    public static void main(String[] args) throws Throwable {
+    public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
+        String testFilePath = "./src/testField/test.txt";
 
         String s = "wqeqw    -  qwe qweqwe - 123123 - --- 123 ----123555";
         s = s.replaceAll("[ -1]","");
         System.out.println(s);
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(testFilePath));
+            writer.write(Integer.toString(111111111));
+
+            writer.close();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+
 
 
 
