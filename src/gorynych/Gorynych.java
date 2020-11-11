3553686898a46106ea962ec6ec9ce997adb007a1
NCK1V <666lion@gmail.com>
2020-11-12 00:08:21 +0300

    12112020

--- src/gorynych/Gorynych.java
+++ src/gorynych/Gorynych.java
@@ -12,6 +12,7 @@ public class Gorynych {
     public static void main(String[] args) {
         //Execution time: 11 ms
         System.out.println("Result: " + getMinutes(3));
+        //Result: 20
     }
 
     private static int getMinutes(int timeAllHours) {
