ea2e2f7ea61f275f74ab041542546f6dd49542a9
NCK1V <666lion@gmail.com>
2020-11-12 15:28:28 +0300

    12112020

--- src/gorynych/Gorynych.java
+++ src/gorynych/Gorynych.java
@@ -27,7 +27,7 @@ public class Gorynych {
         int turnTimeTH = 20;
 
         //каждые 5 минут проверяем куда смотрят все головы. направление одно - +5 минут в копилку
-        for (int i = 0; i <= timeAllMinutes; i += 5) {
+        for (int i = 0; i < timeAllMinutes; i += 5) {
             if (headFirst[currElem(headFirst, turnTimeFH, i)].equals(headSecond[currElem(headSecond, turnTimeSH, i)])
                     && headFirst[currElem(headFirst, turnTimeFH, i)].equals(headThird[currElem(headThird, turnTimeTH, i)])) {
                 timeTogether += 5;
